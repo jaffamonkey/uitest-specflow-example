3e0661b8bdbfb817af52f2e702f8e4474bb2d979
jaffamonkey <paullittlebury@mail.com>
2020-05-23 17:08:34 +0100

    Udpated packages, adjusted specflow code to matach new versions

--- Xamarin.UITest.SpecFlow/Tests/TasksFeature.cs
+++ Xamarin.UITest.SpecFlow/Tests/TasksFeature.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 namespace Xamarin.UITest.SpecFlow.Tests
 {
     [TestFixture(Platform.Android)]
-    [TestFixture(Platform.iOS)]
+    //[TestFixture(Platform.iOS)]
     public partial class TasksFeature : BaseTestFixture
     {
         public TasksFeature(Platform platform)
